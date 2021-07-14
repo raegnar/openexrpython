8923778a15919b78a4c4299fb8c4aa43b2c1edf0
Randall Rauwendaal <randall@rauwendaal.net>
2021-07-14 14:00:54 -0700

    ImathBox.h build fixes

--- setup.py
+++ setup.py
@@ -32,9 +32,9 @@ setup(name='OpenEXR',
   ext_modules=[ 
     Extension('OpenEXR',
               ['OpenEXR.cpp'],
-              include_dirs=['/usr/include/OpenEXR', '/usr/local/include/OpenEXR', '/opt/local/include/OpenEXR'],
+              include_dirs=['/usr/include/OpenEXR', '/usr/local/include/OpenEXR', '/opt/local/include/OpenEXR', '/usr/local/include/Imath'],
               library_dirs=['/usr/local/lib', '/opt/local/lib'],
-              libraries=['Iex', 'Half', 'Imath', 'IlmImf', 'z'],
+              libraries=['Iex', 'Imath', 'OpenEXR', 'z'],
               extra_compile_args=compiler_args)
   ],
   py_modules=['Imath'],
